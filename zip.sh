e1919cc30f3a260b7aa27695e383669498e48ffc
Leo McArdle <leomcardle@gmail.com>
2018-05-30 22:25:12 +0100

    use node 8.10 and update packages

--- zip.sh
+++ zip.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+unset npm_config_prefix
+source /usr/share/nvm/init-nvm.sh
+nvm install 8.10
 mkdir zip
+cp package.json zip/
+cp package-lock.json zip/
 cp index.js zip/
 cp newsletter.js zip/
 cd zip
-mkdir node_modules
-npm install mailparser request cheerio turndown
+NODE_ENV=production npm install
+rm ../post-newsletter.zip
 zip -r ../post-newsletter.zip *
+cp package-lock.json ../
 cd ../
 rm -r zip/
